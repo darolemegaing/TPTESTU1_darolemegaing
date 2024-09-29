a357f268ca7cd3aa29ee4c37c982c8764dc19098
Megaing <verdiane.megaing@gmail.com>
2024-09-29 17:21:59 +0100

    factoriel

--- src/main/com.stjean.operation/ OperationMathematique.java
+++ src/main/com.stjean.operation/ OperationMathematique.java
@@ -2,3 +2,15 @@ package com.stjean.operation;
 public boolean estPositif(int nombre) {
     return nombre > 0;
 }
+
+
+
+
+public int factoriel(int nombre) {
+    if (nombre == 0) return 1;
+    int result = 1;
+    for (int i = 1; i <= nombre; i++) {
+        result *= i;
+    }
+    return result;
+}
